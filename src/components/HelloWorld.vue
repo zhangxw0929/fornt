e13ef3d87bd39d0675e742f5d9d83578185da667
zhangxiaowen0929 <786153809@qq.com>
2023-11-16 11:11:51 +0800

    提一下

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,5 @@
-<script setup lang="ts">
-import {onMounted, ref} from 'vue'
-import axios from "axios";
-onMounted(() => {
-  const userInfo = ref([])
-  axios.get('http://localhost:3000/api/users').then((res:any) => {
-    console.log(res)
-    userInfo.value = res.data
-  })
-})
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1 v-for="item in userInfo">{{ item.name }}</h1>
+  <h1 v-for="item in userInfo">{{ item }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -27,7 +12,7 @@ const count = ref(0)
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
@@ -37,6 +22,24 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+
+
+<script setup lang="ts">
+import {onMounted, ref} from 'vue'
+import axios from "axios";
+const userInfo = ref([])
+onMounted(() => {
+
+  axios.get('http://localhost:3000/api/users').then((res:any) => {
+    console.log(res)
+    userInfo.value = res.data
+  })
+})
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
 
 <style scoped>
 .read-the-docs {
